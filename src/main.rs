527d37758ee06a80348ce40e52803dc38b20f78f
Jake <Jake_Guy_11@protonmail.ch>
2021-11-13 20:17:05 -0500

    tested metadata flags

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use ytd_rs::{YoutubeDL, ResultType, Arg};
+use std::path::PathBuf;
 
 fn main() {
-    println!("Hello, world!");
+    // Set some testing details
+    let test_url = "https://www.youtube.com/watch?v=TIgcVT4SfGw";
+    let output_path = PathBuf::from("./");
+
+    // Define the args
+    let arg_list = vec![Arg::new_with_arg("-f", "bestaudio"), Arg::new_with_arg("--postprocessor-args", "-metadata artist='Mori Calliope'")];
+
+    // Do the actual download
+    let downloader = YoutubeDL::new(&output_path, arg_list, test_url).unwrap();
+    let dl_res = downloader.download();
+
+    // Make sure it's not an error
+    match dl_res.result_type() {
+        ResultType::SUCCESS => println!("Downloaded :)"),
+        _ => println!("Failed to download: {:?}", dl_res.output())
+    };
 }
