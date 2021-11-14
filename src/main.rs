f94beaeace1fb95f69cf96135381866744f02dac
Jake <Jake_Guy_11@protonmail.ch>
2021-11-13 22:15:21 -0500

    added title parsing

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
 use ytd_rs::{YoutubeDL, ResultType, Arg};
 use std::path::PathBuf;
+use std::io::Write;
 
 // A struct that will hold all the data about the song
 #[derive(Debug)]
 struct FileData
 {
-    title: Option<String>,
-    file_name: Option<String>,
-    extension: Option<String>,
-    artist: Option<String>,
-    album: Option<String>,
-    cover: Option<String>,
-    output_path: Option<PathBuf>,
+    title: Option<String>,          // Title is required
+    file_name: Option<String>,      // File name is required
+    extension: Option<String>,      // Extension is required
+    artist: Option<String>,         // Artist is optional
+    album: Option<String>,          // Album is optional
+    cover: Option<String>,          // Cover is optional
+    output_path: Option<PathBuf>,   // Output path is optional
 }
 
 // An enum that will hold all the reasons anything could fail
 #[derive(Debug)]
 enum FailReason
 {
-    FieldNotProvided(String)
+    StdInFailed
 }
 
 fn main() 
@@ -48,6 +49,38 @@ fn main()
 
 fn parse_flags(file_data: &mut FileData) -> Result<(), FailReason>
 {
-    file_data.title = Some(String::from("Title will go here!"));
+    // First, get all the user's arguments as a vector
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    //
+    // Parse the title
+    //
+    
+    // Get the position of the title flag, assign it
+    let title_index_opt = args.iter().position(|i| i.as_str() == "-title" || i.as_str() == "-t");
+    if let Some(flag_index) = title_index_opt
+    {
+        if let Some(requested_title) = args.get(flag_index + 1)
+        {
+            file_data.title = Some(String::from(requested_title));
+        }
+    }
+
+    // If file_data.title is None, there was either no title flag or the user didn't enter a title
+    if file_data.title == None
+    {
+        // Prompt the user to enter a title
+        println!("You didn't enter a title. Please do so now:");
+        print!(">>>");
+        std::io::stdout().flush().unwrap();
+        let mut entered_title = String::new();
+        if let Ok(_) = std::io::stdin().read_line(&mut entered_title)
+        {
+            entered_title.pop();
+            file_data.title = Some(entered_title);
+        } else { return Err(FailReason::StdInFailed); }
+    }
+
     Ok(())
 }
+
